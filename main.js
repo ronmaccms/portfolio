7428e84236eb1e0b8448bfb5218ec57dc0716320
aroncal02 <85313161+aroncal02@users.noreply.github.com>
2024-10-05 15:05:17 -0400

    tests

--- main.js
+++ main.js
@@ -1,41 +1,24 @@
 
-let userAge = Number(prompt("What's your age"))
+// let userAge = Number(prompt("What's your age"))
 
 const app = Vue.createApp({
     data() {
         return {
-            movieTitle: "Shining",
-            age: userAge,
-            //age: 18,
-            showTitle: true,
-            firstName: "Andres",
-            lastName: "Macchiavello",
-            title: "My Portfolio",
-            titleHTML: "Andres Roncal <span class='badge'>Portfolio</span>",
-            alert: 'This is an alert message!',
+            name: "Andres Macchiavello",
+            title: "Portfolio",
             projects: [
-                { title: "portfolio", desc: "Project 1" },
-                { title: "clone", desc: "Project 2" }
-            ],
-            dynamicID: "project_section",
-            dynamicClass: "project",
-            disabled: true,
-            attribute_name: 'href',
-            url: 'https://fabiopacifici.com',
-            event_name: "click",
+                {title: "Portfolio", language: ["HTML", "CSS", "JavaScript", "VueJS"]},
+                {title: "Twitter clone", language: ["HTML", "CSS", "JavaScript", "VueJS"]},
+                {title: "Blog", language: ["HTML", "CSS", "JavaScript", "VueJS"]},
+                {title: "eCommerce", language: ["HTML", "CSS", "JavaScript", "VueJS"]},
+            ]
         };
     },
     methods: {
         runFunction() {
             console.log("Test click function");
         },
-        getFullName(){
-            return this.firstName + " " + this.lastName;
-        },
-        // es5 syntax
-        // getFullName: function(){
 
-        // }
     },
     computed: {
         dynamicEvent() {
