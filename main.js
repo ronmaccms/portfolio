be2eba354ff21c8a98bd3c5fc168d82752010b40
aroncal02 <85313161+aroncal02@users.noreply.github.com>
2024-10-05 13:05:22 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            firstName: "Andres",
+            lastName: "Macchiavello",
+            title: "My Portfolio",
+            titleHTML: "Andres Roncal <span class='badge'>Portfolio</span>",
+            alert: 'This is an alert message!',
+            projects: [
+                { title: "portfolio", desc: "Project 1" },
+                { title: "clone", desc: "Project 2" }
+            ],
+            dynamicID: "project_section",
+            dynamicClass: "project",
+            disabled: true,
+            attribute_name: 'href',
+            url: 'https://fabiopacifici.com',
+            event_name: "click",
+        };
+    },
+    methods: {
+        runFunction() {
+            console.log("Test click function");
+        },
+        getFullName(){
+            return this.firstName + " " + this.lastName;
+        },
+        // es5 syntax
+        // getFullName: function(){
+
+        // }
+    },
+    computed: {
+        dynamicEvent() {
+            return {
+                [this.event_name]: this.runFunction
+            };
+        }
+    }
+});
+
+app.mount('#app');
